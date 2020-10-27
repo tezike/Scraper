405a3d7b46530c7b7522916a328de4720001904d
Ezike Tochukwu <31063326+tezike@users.noreply.github.com>
2020-10-27 01:22:27 +0100

    Add chromedriver

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 apt-get update
 apt-get install python3-pip chromium-chromedriver
+wget https://chromedriver.storage.googleapis.com/87.0.4280.20/chromedriver_linux64.zip
 pip3 install -U fastai fastcore nbdev bs4 selenium fake-useragent --upgrade
 cp /usr/lib/chromium-browser/chromedriver /usr/bin
